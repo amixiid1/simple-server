bc82b8c5852090a2584e999dccf6189142fb1cc8
amixiid1 <amixiid0@gmail.com>
2026-03-31 12:11:55 +0300

    first practice

---
+++ server.js
@@ -0,0 +1,45 @@
+const cors = require('cors')
+const express = require("express");
+const app = express();
+
+app.use(cors({
+    origin:['http://localhost:5500', 'http://127.0.0.1:5500']
+}))
+
+app.get("/", (req, res) => {
+  res.send("hello get is working");
+});
+
+app.get("/about", (req, res) => {
+  res.send("this is about page");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("this is the contact page");
+});
+
+app.get("/products", (req, res) => {
+  res.json([
+    { id: 1, name: "Laptop", price: 1299 },
+    { id: 2, name: "Mouse", price: 50 },
+  ]);
+});
+
+app.get("/products/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const products = [
+    { id: 1, name: "Laptop", price: 1299 },
+    { id: 2, name: "Mouse", price: 50 },
+  ];
+  const requestedProduct = products.find((product) => product.id === id);
+  res.json(requestedProduct)
+});
+
+app.get("/message",(req,res)=>{
+    res.json({message:"Hello from your express backend 😊"})
+})
+
+app.listen(3000, () => {
+  console.log("The server is running");
+});
